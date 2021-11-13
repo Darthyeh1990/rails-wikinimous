3a9f878766a865bf20f13eac9b1d361da1eb87ab
Timothy Yeh <jcluvt@yahoo.com>
2021-11-13 16:00:33 +0800

    complete rails wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :articles
+
+  # Read (all)
+  get '/articles', to: 'articles#index'
+
+  # Create Step 1
+  get 'articles/new', to: 'articles#new'
+  # Create Step 2
+  post 'articles', to: 'articles#create'
+
+  # Read (one)
+  get '/articles/:id', to: 'articles#show', as: :article
+
+  # Update Step 1
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # Update Step 2
+  patch 'articles/:id', to: 'articles#update'
+
+  # Delete
+  delete 'articles/:id', to: 'articles#destroy'
 end
